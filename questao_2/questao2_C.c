8aa59102090907c497c91e343ddda0b601b3b429
mirleims <43613145+mirleims@users.noreply.github.com>
2023-06-12 20:22:53 -0300

    Add files via upload

---
+++ questao_2/questao2_C.c
@@ -0,0 +1,34 @@
+/*  ALUNO: REYNAN DA SILVA DIAS PAIVA
+    N° MATRICULA: 221115751
+*/
+#include <stdio.h>
+#include <math.h>
+
+float media_aritmetica(float a, float b, float c) {
+    return ((a + b + c) / 3);
+}
+
+float media_geometrica(float a, float b, float c) {
+    return pow((a * b * c), (1.0 / 3.0));
+}
+
+float media_harmonica(float a, float b, float c) {
+    return (3.0 / ((1.0 / a) + (1.0 / b) + (1.0 / c)));
+}
+
+int main() {
+    float a, b, c;
+    
+    printf("Digite o primeiro valor: ");
+    scanf("%f", &a);
+    printf("Digite o segundo valor: ");
+    scanf("%f", &b);
+    printf("Digite o terceiro valor: ");
+    scanf("%f", &c);
+    printf("\nDado os valores: %.2f %.2f %.2f", a, b, c);
+    printf("\nA média aritmética é: %.2f\n", media_aritmetica(a, b, c));
+    printf("A média geométrica é: %.2f\n", media_geometrica(a, b, c));
+    printf("A média harmônica é: %.2f\n", media_harmonica(a, b, c));
+    
+    return 0;
+}
